ba19753682af5f2ff45c23e1f40f469f62c93dee
Jerry Lee <jerry.lee09@yahoo.com>
2016-09-01 20:07:19 -0500

    fix select

--- server/public/scripts/controllers/hero-controller.js
+++ server/public/scripts/controllers/hero-controller.js
@@ -2,37 +2,19 @@ app.controller('HeroController', ['$scope', '$http', function ($scope, $http) {
   $scope.heros = []; // the array of heros we expect
   $scope.newHero = {}; // a single hero to be added to db
 
-
-
   getheros();
-  $scope.OrderBySelect = 'title';
+  $scope.OrderBySelect = 'alias';
 
-
-  $scope.superpowers = [{
-    id: 1,
-    power_name: 'Invisibility'
-  }, {
-    id: 2,
-    power_name: 'Flight'
-  }, {
-    id: 3,
-    power_name: 'Super Speed'
-  }, {
-    id: 4,
-    power_name: 'Heat Vision'
-  }, {
-    id: 5,
-    power_name: 'Super Strength'
-  }, {
-    id: 6,
-    power_name: 'Power Blast'
-  }, {
-    id: 7,
-    power_name: 'Animal Affinity'
-  }, {
-    id: 8,
-    power_name: 'Accelerated Healing'
-  }];
+$scope.options = {
+  "power_name_1": "Flight",
+  "power_name_2": "Invisibility",
+  "power_name_3": "Accelerated Healing",
+  "power_name_4": "Super Strength",
+  "power_name_5": "Super Speed",
+  "power_name_6": "Power Blast",
+  "power_name_7": "Heat Vision",
+  "power_name_8": "Animal Affinity"
+}
 
   /** --- Scoped functions -- **/
   $scope.submitNewHero = function () {
@@ -51,8 +33,6 @@ app.controller('HeroController', ['$scope', '$http', function ($scope, $http) {
         getheros();
       });
   };
-
-
 
   /** -- Utility functions -- **/
   function getheros() {
